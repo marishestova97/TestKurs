804919095f4c9fe6a3c9c0b4df4221715570081f
Mary <lllavrucha1308@yandex.ru>
2017-05-06 11:37:35 +0300

    init

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,25 @@
+package hello;
+
+/**
+ * Created by USER on 03.05.2017.
+ */
+public class Greeting {
+    private long id;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
